bef62631d0418a2c8c47b3ed22eb7da9a3a3a1dc
bless9111 <svs763@mail.ru>
2025-04-24 14:37:25 +0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,63 +1,33 @@
 let one = $('#one');
+let containerOne = $('#containerA');
+let three = $('#three');
+let containerThree = $('#containerC'); 
 
-	let containerOne = $('#containerA');
+//Scroll to bottom on start
+scrollToBottom(containerOne, one);
+scrollToBottom(containerThree, three);
 
-	let three = $('#three');
-
-	let containerThree = $('#containerC'); 
-
-	
-
-	//Scroll to bottom on start
-
-	scrollToBottom(containerOne, one);
-
-	scrollToBottom(containerThree, three);
-
-	
-
-	//on Scroll make the move
-
-	$(window).scroll(function() {
-
-	    inverseScroll(containerOne, one);
-
+//on Scroll make the move
+$(window).scroll(function() {
+	inverseScroll(containerOne, one);
         inverseScroll(containerThree, three);
+});
 
-	});
+function inverseScroll(main,target){
+	let window_height = $(window).height();
+	let scrollTop = $(window).scrollTop();
+	let scrollPercent = scrollTop / ($(document).height() - window_height);
+	let targetPercent = 1 - scrollPercent;
+	let height =target.height();
+	let offset = target.offset().top;
+	let targeOffset = (height - window_height) * targetPercent;
+	$(main).scrollTop(targeOffset);     	   
 
+}
 
-
-	function inverseScroll(main,target){
-
-		let window_height = $(window).height();
-
-		let scrollTop = $(window).scrollTop();
-
-		let scrollPercent = scrollTop / ($(document).height() - window_height);
-
-		let targetPercent = 1 - scrollPercent;
-
-		let height =target.height();
-
-		let offset = target.offset().top;
-
-		let targeOffset = (height - window_height) * targetPercent;
-
-		$(main).scrollTop(targeOffset);     	   
-
-	}
-
-	
-
-	
-
-	function scrollToBottom(main,target){
-
-		let height =target.height();
-
-		let div_offset = target.offset().top;
-
-		let window_height = $(window).height();
-
-		$(main).scrollTop(height - window_height);    
+function scrollToBottom(main,target) {
+	let height =target.height();
+	let div_offset = target.offset().top;
+	let window_height = $(window).height();
+	$(main).scrollTop(height - window_height);    
+}
