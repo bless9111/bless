55b4435ea98d71178d4c15a17edd39a3d5ab6cab
bless9111 <svs763@mail.ru>
2025-05-01 05:04:41 +0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -6,11 +6,11 @@ let containerThree = $('#containerC');
 //Scroll to bottom on start
 scrollToBottom(containerOne, one);
 scrollToBottom(containerThree, three);
-
+/*
 $('html, body').animate({ 
-	scrollTop: $('.homeContent').offset().top
-}, 0);
-
+	scrollTop: $('.homeContainer').offset().top
+}, 0);*/
+$('.homeContainer').scroll();
 //on Scroll make the move
 $(window).scroll(function() {
 	inverseScroll(containerOne, one);
